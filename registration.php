e25e22d259dac0d01cd13fd00f521af9350debbd
Chris <Chris@192.168.1.120>
2017-07-01 15:23:43 +0200

    Make ready for magento connect

--- registration.php
+++ registration.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * Copyright Â© 2017 FinalBit Solution. All rights reserved.
- * http://www.radicalwear.ch
+ * Copyright © 2017 FinalBit Solution. All rights reserved.
+ * http://www.finalbit.ch
  * See LICENSE.txt for license details.
  */
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
-    'FinalBitSW_de_ch',
+    'FinalBit_de_ch',
     __DIR__
 );
